71b724bb8516e28e65b6bcf69182dcbfd4b27bfc
angversh <aavershinina_1@edu.hse.ru>
2023-06-17 12:38:19 +0300

    Add files via upload

---
+++ f/Postfix/Postfix/Infix.cpp
@@ -0,0 +1,299 @@
+﻿#pragma once
+#include <iostream>
+#include <string> 
+#include <limits> 
+#include "List.cpp"
+#include "Postfix.cpp"
+using namespace std;
+
+class InfixCalc {
+public:
+    bool IsOverflow, IsBadSeq, IsBadElem, IsCorrect;
+    double num;
+    bool flag;
+    char tmp;
+    InfixCalc() {
+        this->IsOverflow = false;
+        this->IsBadSeq = false;
+        this->IsBadElem = false;
+        this->IsCorrect = true;
+        this->num = 0;
+        this->flag = false;
+        this->tmp = '\0';
+    }
+    int OpPriority(char Op) {
+        if (Op == '+' || Op == '-') {
+            return 1;
+        }
+        else {
+            return 2;
+        }
+    }
+    void IsGood(string s) {
+        IsCorrect = true;
+        IsBadElem = false;
+        if (s.size() == 0) {
+            IsCorrect = false;
+            return;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if (!isspace(s[i])) {
+                break;
+            }
+            if (isspace(s[i]) && i == s.size() - 1) {
+                IsCorrect = false;
+                return;
+            }
+        }
+        int ind = 0;
+        int indLastDig = -1, indLastOp = -1, indLastOpenBr = -1, indLastClosBr = -1;
+        while (isspace(s[ind])) {
+            ind++;
+        }
+        if (s[ind] == '+' || s[ind] == '-' || s[ind] == '*' || s[ind] == '/') {
+            IsCorrect = false;
+            return;
+        }
+        ind = s.size() - 1;
+        while (isspace(s[ind])) {
+            ind--;
+        }
+        if (s[ind] == '+' || s[ind] == '-' || s[ind] == '*' || s[ind] == '/') {
+            IsCorrect = false;
+            return;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if (!isdigit(s[i]) && !isspace(s[i]) && s[i] != '(' && s[i] != ')' && s[i] != '+' && s[i] != '-' && s[i] != '*' && s[i] != '/') {
+                IsCorrect = false;
+                IsBadElem = true;
+                return;
+            }
+            if ((s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/') && indLastOp >= 0 && !(indLastDig > indLastOp && indLastDig < i)) {
+                IsCorrect = false;
+                return;
+            }
+            if (isdigit(s[i]) && indLastDig >= 0 && indLastDig != i - 1 && !(indLastOp > indLastDig && indLastOp < i)) {
+                IsCorrect = false;
+                return;
+            }
+            if ((s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/') && indLastOpenBr >= 0) {
+                if (!(indLastClosBr > indLastOpenBr && indLastClosBr < i) && !(indLastOp > indLastOpenBr && indLastOp < i) && !(indLastDig > indLastOpenBr && indLastDig < i)) {
+                    IsCorrect = false;
+                    return;
+                }
+            }
+            if (s[i] == ')' && indLastOp >= 0) {
+                if (!(indLastOpenBr > indLastOp && indLastOpenBr < i) && !(indLastClosBr > indLastOp && indLastClosBr < i) && !(indLastDig > indLastOp && indLastDig < i)) {
+                    IsCorrect = false;
+                    return;
+                }
+            }
+            if (s[i] == ')' && indLastOpenBr >= 0) {
+                if (!(indLastClosBr > indLastOpenBr && indLastClosBr < i) && !(indLastDig > indLastOpenBr && indLastDig < i) && !(indLastOp > indLastOpenBr && indLastOp < i)) {
+                    IsCorrect = false;
+                    return;
+                }
+            }
+            if (s[i] == '(' && indLastClosBr >= 0) {
+                if (!(indLastOpenBr > indLastClosBr && indLastOpenBr < i) && !(indLastDig > indLastClosBr && indLastDig < i) && !(indLastOp > indLastClosBr && indLastOp < i)) {
+                    IsCorrect = false;
+                    return;
+                }
+            }
+            if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/') {
+                indLastOp = i;
+            }
+            else if (s[i] == '(') {
+                indLastOpenBr = i;
+            }
+            else if (s[i] == ')') {
+                indLastClosBr = i;
+            }
+            else if (isdigit(s[i])) {
+                indLastDig = i;
+            }
+        }
+    }
+    void InfToPostf(string expr, MyStack& Stack, MyQueue& Queue) {
+        num = 0;
+        tmp = '\0';
+        flag = false;
+        IsOverflow = false;
+        IsBadSeq = false;
+        Stack.Clear();
+        Queue.Clear();
+        for (int j = 0; j < expr.size(); j++) {
+            if (isdigit(expr[j])) {
+                if (num <= std::numeric_limits<int>::max() / 10) {
+                    num *= 10;
+                }
+                else {
+                    IsOverflow = true;
+                    return;
+                }
+                if (num <= std::numeric_limits<int>::max() - (expr[j] - '0')) {
+                    num += (expr[j] - '0');
+                }
+                else {
+                    IsOverflow = true;
+                    return;
+                }
+                flag = true;
+                if (j == expr.size() - 1) {
+                    Queue.Push(new DoubleEl(num), Queue.HeadNode);
+                    num = 0;
+                }
+            }
+            else if (isspace(expr[j])) {
+                if (flag) {
+                    Queue.Push(new DoubleEl(num), Queue.HeadNode);
+                    num = 0;
+                    flag = false;
+                }
+            }
+            else if (expr[j] == '+' || expr[j] == '-' || expr[j] == '*' || expr[j] == '/') {
+                if (flag) {
+                    Queue.Push(new DoubleEl(num), Queue.HeadNode);
+                    num = 0;
+                    flag = false;
+                }
+                if (Stack.EmptyStack() || dynamic_cast<CharEl*>(Stack.Top())->getElement() == '(') {
+                    Stack.Push(new CharEl(expr[j]), Stack.HeadNode);
+                }
+                else if (OpPriority(expr[j]) > OpPriority(dynamic_cast<CharEl*>(Stack.Top())->getElement())) {
+                    Stack.Push(new CharEl(expr[j]), Stack.HeadNode);
+                }
+                else if (OpPriority(expr[j]) <= OpPriority(dynamic_cast<CharEl*>(Stack.Top())->getElement())) {
+                    while (!Stack.EmptyStack() && dynamic_cast<CharEl*>(Stack.Top())->getElement() != '(' && OpPriority(dynamic_cast<CharEl*>(Stack.Top())->getElement()) >= OpPriority(expr[j])) {
+                        tmp = dynamic_cast<CharEl*>(Stack.Top())->getElement();
+                        Queue.Push(new CharEl(tmp), Queue.HeadNode);
+                        Stack.Pop();
+                    }
+                    Stack.Push(new CharEl(expr[j]), Stack.HeadNode);
+                }
+            }
+            else if (expr[j] == '(') {
+                if (flag) {
+                    Queue.Push(new DoubleEl(num), Queue.HeadNode);
+                    num = 0;
+                    flag = false;
+                }
+                Stack.Push(new CharEl(expr[j]), Stack.HeadNode);
+            }
+            else if (expr[j] == ')') {
+                if (flag) {
+                    Queue.Push(new DoubleEl(num), Queue.HeadNode);
+                    num = 0;
+                    flag = false;
+                }
+                while (!Stack.EmptyStack() && dynamic_cast<CharEl*>(Stack.Top())->getElement() != '(') {
+                    tmp = dynamic_cast<CharEl*>(Stack.Top())->getElement();
+                    Queue.Push(new CharEl(tmp), Queue.HeadNode);
+                    Stack.Pop();
+                }
+                if (Stack.EmptyStack()) {
+                    IsBadSeq = true;
+                    return;
+                }
+                else {
+                    Stack.Pop();
+                }
+            }
+        }
+        while (!Stack.EmptyStack()) {
+            if (dynamic_cast<CharEl*>(Stack.Top())->getElement() == '(') {
+                IsBadSeq = true;
+                return;
+            }
+            else {
+                tmp = dynamic_cast<CharEl*>(Stack.Top())->getElement();
+                Queue.Push(new CharEl(tmp), Queue.HeadNode);
+                Stack.Pop();
+            }
+        }
+    }
+};
+int input() {
+    long long input;
+    while (1) {
+        cin >> input;
+        if (cin.fail() || cin.peek() != '\n' || abs(input) > std::numeric_limits<int>::max()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        else {
+            break;
+        }
+    }
+    return int(input);
+}
+int main() {
+    MyStack Stack;
+    MyQueue Queue;
+    InfixCalc Inf;
+    PostfixCalc Postf;
+    string expr;
+    int cmd;
+    cout << "Infix Calculator" << endl;
+    cout << "Menu:" << endl;
+    cout << "1. Count the expression" << endl;
+    cout << "2. Exit" << endl;
+    while (1) {
+        cmd = input();
+        if (cmd == 1) {
+            cout << "Enter the expression: " << endl;
+            cin.ignore();
+            getline(cin, expr);
+            cout << "Your expression is: " << expr << endl;
+            Inf.IsGood(expr);
+            if (!Inf.IsCorrect) {
+                cout << "Incorrect expression!" << endl;
+                if (Inf.IsBadElem) {
+                    cout << "Unsupported element detected!" << endl;
+                }
+            }
+            else {
+                Inf.InfToPostf(expr, Stack, Queue);
+                if (Inf.IsBadSeq == 0 && Inf.IsOverflow == 0) {
+                    cout << "Postfix expression: " << endl;
+                    Queue.print();
+                    Stack.Clear();
+                    Postf.CountExpr(Stack, Queue);
+                    if (Postf.IsCorrect == 0) {
+                        cout << "Incorrect expression!" << endl;
+                    }
+                    else if (Postf.IsOverflow) {
+                        cout << "Oveflow result!" << endl;
+                    }
+                    else if (Postf.IsDBZ) {
+                        cout << "Division by zero!" << endl;
+                    }
+                    else {
+                        if (Postf.ans == static_cast<int>(Postf.ans)) {
+                            cout << "The answer is: " << fixed << static_cast<int>(Postf.ans) << endl;
+                        }
+                        else {
+                            cout << "The answer is: " << defaultfloat << Postf.ans << endl;
+                        }
+                    }
+                    Postf.ClearCalc();
+                }
+                else {
+                    cout << "Invalid expression!" << endl;
+                    if (Inf.IsBadSeq == 1) {
+                        cout << "Mismatched brackets!" << endl;
+                    }
+                    if (Inf.IsOverflow == 1) {
+                        cout << "Operand oveflow!" << endl;
+                    }
+                }
+            }
+        }
+        else if (cmd == 2) {
+            return 0;
+        }
+        else {
+            cout << "Incorrect menu command!" << endl;
+        }
+    }
+}
---
+++ f/Postfix/Postfix/Postfix.cpp
@@ -0,0 +1,195 @@
+﻿#include <iostream> 
+#include <string>
+#include <limits>
+#include "Infix.cpp"
+#include "C:\Users\User\source\repos\Postfix\Postfix\List.cpp"
+
+using namespace std;
+
+class MyStack : public MyList {  //стек и односв. список
+public:
+	MyStack() {
+		HeadNode = NULL;
+	}
+	~MyStack() {
+		Clear();
+	} 
+	//stack methods
+
+	int EmptyStack() {
+		return IsEmpty();
+	}
+	int StackSize() {
+		return ListSize();
+	}
+	void Clear() {
+		DeleteList();
+	}
+	void Push(ListEl* a, Node*& HeadNode) {
+		AddEnd(a, HeadNode);
+	}
+	void Pop() {
+		DeletePosition(StackSize() - 1, HeadNode);
+	}
+	ListEl* Top() {
+		return FindPosition(StackSize() - 1);
+	}
+
+};
+
+class MyQueue : public MyList {   // очередь и односв. список
+public:
+	MyQueue() {
+		HeadNode = NULL;
+	}
+	~MyQueue() {
+		Clear();
+	}
+	void Clear() {
+		DeleteList();
+	}
+	int EmptyQueue() {
+		return IsEmpty();
+	}
+	int QueueSize() {
+		return ListSize();
+	}
+	void Push(ListEl* a, Node*& HeadNode) {
+		AddEnd(a, HeadNode);
+	}
+	void Pop() {
+		DeletePosition(0, HeadNode);
+	}
+	ListEl* Head() {
+		return FindPosition(0);
+	}
+	ListEl* Tail() {
+		return FindPosition(QueueSize() - 1);
+	}
+};
+
+class PostfixCalc {
+public:
+	double res, ans;
+	int ErrCode; //0 - No error, 1 - Operation overflow, 2 - Division by Zero
+	bool IsCorrect, IsOverflow, IsDBZ;
+	PostfixCalc() {
+		this->IsCorrect = true;
+		this->IsOverflow = false;
+		this->IsDBZ = false;
+		this->res = 0;
+		this->ans = 0;
+		this->ErrCode = 0;
+	}
+	void MakeOp(double a, double b, char op) {
+		switch (op) {
+		case '+':
+			if ((b > 0 && a > std::numeric_limits<int>::max() - b) || (b < 0 && a < std::numeric_limits<int>::min() - b)) {
+				this->ErrCode = 1;
+			}
+			else {
+				this->res = a + b;
+			}
+			break;
+		case '-':
+			if ((b < 0 && a > std::numeric_limits<int>::max() + b) || (b > 0 && a < std::numeric_limits<int>::min() + b)) {
+				this->ErrCode = 1;
+			}
+			else {
+				this->res = a - b;
+			}
+			break;
+		case '*':
+			if (a == 0 || b == 0) {
+				this->res = 0;
+			}
+			else if ((b > 0) && ((a > std::numeric_limits<int>::max() / b) || (a < std::numeric_limits<int>::min() / b))) {
+				this->ErrCode = 1;
+			}
+			else if ((b < 0) && ((a < std::numeric_limits<int>::max() / b) || (a > std::numeric_limits<int>::min() / b))) {
+				this->ErrCode = 1;
+			}
+			else {
+				this->res = a * b;
+			}
+			break;
+		case '/':
+			if (b == 0) {
+				this->ErrCode = 2;
+			}
+			else if ((b > 0) && ((a > std::numeric_limits<int>::max() * b) || (a < std::numeric_limits<int>::min() * b))) {
+				this->ErrCode = 1;
+			}
+			else if ((b < 0) && ((a < std::numeric_limits<int>::max() * b) || (a > std::numeric_limits<int>::min() * b))) {
+				this->ErrCode = 1;
+			}
+			else {
+				this->res = a / b;
+			}
+			break;
+		}
+	}
+	void ClearOps() {
+		this->res = 0;
+		this->ErrCode = 0;
+	}
+	void CountExpr(MyStack& Stack, MyQueue& Queue) {
+		double num1, num2;
+		Stack.Clear();
+		ClearOps();
+		while (!Queue.EmptyQueue()) {
+			if (dynamic_cast<DoubleEl*>(Queue.Head()) != NULL) {
+				Stack.Push(dynamic_cast<DoubleEl*>(Queue.Head()), Stack.HeadNode);
+				Queue.Pop();
+			}
+			else {
+				if (Stack.EmptyStack()) {
+					IsCorrect = false;
+					return;
+				}
+				else {
+					num2 = dynamic_cast<DoubleEl*>(Stack.Top())->getElement();
+					Stack.Pop();
+				}
+				if (Stack.EmptyStack()) {
+					IsCorrect = false;
+					return;
+				}
+				else {
+					num1 = dynamic_cast<DoubleEl*>(Stack.Top())->getElement();
+					Stack.Pop();
+				}
+				MakeOp(num1, num2, dynamic_cast<CharEl*>(Queue.Head())->getElement());
+				if (ErrCode == 0) {
+					Stack.Push(new DoubleEl(res), Stack.HeadNode);
+					ClearOps();
+					Queue.Pop();
+				}
+				else if (ErrCode == 1) {
+					IsOverflow = true;
+					ClearOps();
+					return;
+				}
+				else {
+					IsDBZ = true;
+					ClearOps();
+					return;
+				}
+			}
+		}
+		if (Stack.StackSize() != 1) {
+			IsCorrect = 0;
+		}
+		else {
+			ans = dynamic_cast<DoubleEl*>(Stack.Top())->getElement();
+		}
+	}
+	void ClearCalc() {
+		this->IsCorrect = true;
+		this->IsOverflow = false;
+		this->IsDBZ = false;
+		this->res = 0;
+		this->ans = 0;
+		this->ErrCode = 0;
+	}
+};
